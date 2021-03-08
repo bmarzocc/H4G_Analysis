e9e7546347aa0161cd9e3902b1b04f68b0d4d838
wamorkart <wamorkar.t@husky.neu.edu>
2021-03-08 14:29:19 +0100

    bdt evaluation script

--- BDTTraining/ApplyTraining/ApplyCatBDT.py
+++ BDTTraining/ApplyTraining/ApplyCatBDT.py
@@ -4,12 +4,14 @@ import sys, getopt
 
 # import argparse
 if __name__ == '__main__':
+
     File = sys.argv[1]
     mass = sys.argv[2]
-    doSystematics = sys.argv[3]
-    year = sys.argv[4]
-    Weight = sys.argv[5]
-    Out = sys.argv[6]
+    genMass = sys.argv[3]
+    doSystematics = sys.argv[4]
+    year = sys.argv[5]
+    Weight = sys.argv[6]
+    Out = sys.argv[7]
 
     reader = TMVA.Reader()
 
@@ -21,31 +23,44 @@ if __name__ == '__main__':
     p2mva = array('f',[0])
     p3mva = array('f',[0])
     p4mva = array('f',[0])
-    a1_a2_dR = array('f',[0])
-    a1_pt_Over_tp_mass = array('f',[0])
-    a2_pt_Over_tp_mass = array('f',[0])
-    a1mass = array('f',[0])
-    a2mass = array('f',[0])
-
+    #a1_a2_dR = array('f',[0])
+    a1_a2_dR_over_tp_mass = array('f',[0])
+    #a1mass_over_tp_mass = array('f',[0])
+    #a2mass_over_tp_mass = array('f',[0])
+    #a1_pt_Over_tp_mass = array('f',[0])
+    #a2_pt_Over_tp_mass = array('f',[0])
+    a1mass_minus_genMass_over_tp_mass = array('f',[0])
+    a2mass_minus_genMass_over_tp_mass = array('f',[0])
+    #a1mass = array('f',[0])
+    #a2mass = array('f',[0])
+    #genMass_arr = array('f',[0])
+    #genMass = array('f',genMass)
     reader.AddVariable('pho1_MVA<-1.? -1.1: pho1_MVA',p1mva)
     reader.AddVariable('pho2_MVA<-1.? -1.1: pho2_MVA',p2mva)
     reader.AddVariable('pho3_MVA<-1.? -1.1: pho3_MVA',p3mva)
     reader.AddVariable('pho4_MVA<-1.? -1.1: pho4_MVA',p4mva)
     reader.AddVariable('a1_mass_dM-a2_mass_dM',diffdM)
-    reader.AddVariable('a1_mass_dM',a1mass)
-    reader.AddVariable('a2_mass_dM',a2mass)
+   # reader.AddVariable('a1_mass_dM/tp_mass',a1mass_over_tp_mass)
+   # reader.AddVariable('a2_mass_dM/tp_mass',a2mass_over_tp_mass)
+    #reader.AddVariable('a1_mass_dM',a1mass)
+    #reader.AddVariable('a2_mass_dM',a2mass)
+    reader.AddVariable('(a1_mass_dM-genMass)/tp_mass',a1mass_minus_genMass_over_tp_mass)
+    reader.AddVariable('(a2_mass_dM-genMass)/tp_mass',a2mass_minus_genMass_over_tp_mass)
     reader.AddVariable('cosTheta_a1_dM',ct1)
     reader.AddVariable('a1_pt_dM',a1pt)
     reader.AddVariable('a2_pt_dM',a2pt)
-    reader.AddVariable('a1_a2_dR_dM',a1_a2_dR)
-    reader.AddVariable('a1_pt_dM/tp_mass',a1_pt_Over_tp_mass)
-    reader.AddVariable('a2_pt_dM/tp_mass',a2_pt_Over_tp_mass)
+    reader.AddVariable('a1_a2_dR_dM/tp_mass',a1_a2_dR_over_tp_mass)
+    #reader.AddVariable('a1_a2_dR_dM',a1_a2_dR)
+    #reader.AddVariable('a1_pt_dM/tp_mass',a1_pt_Over_tp_mass)
+    #reader.AddVariable('a2_pt_dM/tp_mass',a2_pt_Over_tp_mass)
+    #reader.AddVariable('genMass',genMass_arr)
 
     reader.BookMVA("BDT",Weight)
-
-
     systLabels = [""]
-    if (doSystematics and 'signal' in File):
+    #print doSystematics
+    #if 'signal' in File: print "File:", File
+    if (doSystematics  and 'signal' in File):
+        #print "here"
         for direction in ["Up","Down"]:
                systLabels.append("MvaShift%s01sigma"%direction)
                systLabels.append("SigmaEOverEShift%s01sigma"%direction)
@@ -77,7 +92,7 @@ if __name__ == '__main__':
     FilesToRedo = File.split(',')
     treelist = []
     for f in FilesToRedo:
-        # print f
+        print f
         infilename =  f
         infile = TFile(infilename)
         # print "file: ", infile
@@ -97,6 +112,19 @@ if __name__ == '__main__':
                 elif (year == "2017" or year == "2018"):
                      tree = infile.Get("HAHMHToAA_AToGG_MA_"+str(mass)+"GeV_TuneCP5_PSweights_13TeV_madgraph_pythia8_13TeV_H4GTag_0" + systLabel)
                      treelist.append(tree)
+        elif ('DiPho_40to80' in f):
+             tree = infile.Get("DiPhotonJetsBox_M40_80_Sherpa_13TeV_H4GTag_0")
+             tree.SetBranchStatus("bdt",0)
+             treelist.append(tree)
+        elif ('DiPho_80toInf' in f):
+             # print "here"
+             tree = infile.Get("DiPhotonJetsBox_MGG_80toInf_13TeV_Sherpa_13TeV_H4GTag_0")
+             tree.SetBranchStatus("bdt",0)
+             treelist.append(tree)
+        elif ('DataDriven' in f):
+            tree = infile.Get('tree')
+            tree.SetBranchStatus("bdt",0)
+            treelist.append(tree)
         elif ('mix' in f):
              tree = infile.Get("Data_13TeV_H4GTag_0")
              treelist.append(tree)
@@ -118,10 +146,10 @@ if __name__ == '__main__':
                 a1pt[0] = float(tree.a1_pt_dM)
                 a2pt[0] = float(tree.a2_pt_dM)
                 if (tree.pho1_MVA<-1.):
-                    p1mva[0] = -1.1
+                     p1mva[0] = -1.1
                 else: p1mva[0] =float(tree.pho1_MVA)
                 if (tree.pho2_MVA<-1.):
-                    p2mva[0] = -1.1
+                     p2mva[0] = -1.1
                 else: p2mva[0] =float(tree.pho2_MVA)
                 if (tree.pho3_MVA<-1.):
                     p3mva[0] = -1.1
@@ -130,13 +158,20 @@ if __name__ == '__main__':
                     p4mva[0] = -1.1
                 else: p4mva[0] =float(tree.pho4_MVA)
 
-                a1_a2_dR[0] = float(tree.a1_a2_dR_dM)
-                a1_pt_Over_tp_mass[0] = float(float(tree.a1_pt_dM)/float(tree.tp_mass))
-                a2_pt_Over_tp_mass[0] = float(float(tree.a2_pt_dM)/float(tree.tp_mass))
-                a1mass[0] = float(tree.a1_mass_dM)
-                a2mass[0] = float(tree.a2_mass_dM)
-
+                #a1_a2_dR[0] = float(tree.a1_a2_dR_dM)
+                a1_a2_dR_over_tp_mass[0] = float(tree.a1_a2_dR_dM)/float(tree.tp_mass)
+                a1mass_minus_genMass_over_tp_mass[0] = (float(tree.a1_mass_dM)-float(genMass))/float(tree.tp_mass)
+                a2mass_minus_genMass_over_tp_mass[0] = (float(tree.a2_mass_dM)-float(genMass))/float(tree.tp_mass)
+                #a1_pt_Over_tp_mass[0] = float(float(tree.a1_pt_dM)/float(tree.tp_mass))
+                #a2_pt_Over_tp_mass[0] = float(float(tree.a2_pt_dM)/float(tree.tp_mass))
+                #a1mass_over_tp_mass[0] = float(tree.a1_mass_dM)/float(tree.tp_mass)
+                #a2mass_over_tp_mass[0] = float(tree.a2_mass_dM)/float(tree.tp_mass)
+                #a1mass[0] = float(tree.a1_mass_dM)
+                #a2mass[0] = float(tree.a2_mass_dM)
+                #genMass_arr[0] = float(genMass)
+                #print genMass_arr
                 bdt[0] = reader.EvaluateMVA("BDT")
+                #print bdt[0]
 
                 outtree.Fill()
             outfile.cd()
--- BDTTraining/ApplyTraining/runApplyCatBDT_Condor.py
+++ BDTTraining/ApplyTraining/runApplyCatBDT_Condor.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
   # # parser.add_option(   "-i", "--input",     dest="input",     default="",   type="string", help="input root file" )
   # parser.add_option(   "-m", "--max",     dest="max",     default="",   type="string", help="max" )
   # # parser.add_option(   "-y", "--year",     dest="year",     default="",   type="string", help="year" )
-  # # parser.add_option(   "-o", "--output",     dest="output",     default="",   type="string", help="output" )
+  # # parser.add_opction(   "-o", "--output",     dest="output",     default="",   type="string", help="output" )
   #
   #
   # (options, args) = parser.parse_args()
@@ -82,38 +82,101 @@ queue arguments from arguments.txt
   outputDir = os.getcwd()
 
   script = '''#!/bin/sh -e
-INPUTFILE=$1;
-MASS=$2;
-SYSTEMATICS=$3;
-YEAR=$4;
-WEIGHT=$5;
-OUTPUT=$6;
-python ApplyCatBDT.py ${INPUTFILE} ${MASS} ${SYSTEMATICS} ${YEAR} ${WEIGHT} ${OUTPUT}
+LOCAL=$1;
+INPUTFILE=$2;
+MASS=$3;
+GENMASS=$4;
+SYSTEMATICS=$5;
+YEAR=$6;
+WEIGHT=$7;
+OUTPUT=$8;
+python ${LOCAL}/ApplyCatBDT.py ${INPUTFILE} ${MASS} ${GENMASS} ${SYSTEMATICS} ${YEAR} ${WEIGHT} ${OUTPUT}
 echo -e "DONE";
 '''
   arguments=[]
 
-
-  year = [2016, 2017, 2018]
-  mass = [60, 55, 50, 45, 40, 30, 25, 20, 15]
+  year = [2018]
+  #year = [2016, 2017, 2018]
+  #mass = [60]
+  # mass_training = 55
+  #mass = [60, 55, 50, 45, 40, 35,30, 25, 20, 15]
+  mass = [25]
   doSystematics = 1
-  weightFile = '/eos/user/b/bmarzocc/H4G/dataset_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgData_m'
-  outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Dec2020/dataset_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgData/'
-  inputfile_signal = '/eos/user/t/twamorka/H4G_SignalSamples_MGP/'
+  # weightFile = '/eos/user/b/bmarzocc/H4G/dataset_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd_m'
+  # weightFile = '/eos/user/b/bmarzocc/H4G/dataset_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd_m'
+  #weightFile = '/eos/user/b/bmarzocc/H4G/dataset_PhoMVA_manyKinVars_aMass_fullRun2_DataDriven_crA_MGPodd_m'
+
+  # outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/dataset_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd_newSignalWeights_noTails/'
+
+  # outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/dataset_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd/SignalInterpol_M55/'
+  #outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/dataset_PhoMVA_manyKinVars_aMass_fullRun2_DataDriven_crA_MGPodd/'
+
+  # ## per-mass point training without m_a variables
+  #weightFile = '/afs/cern.ch/work/t/twamorka/H4G_Analysis/BDTTraining/PerformTraining/PerMassPoint_WithoutMa_M55'
+  #outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/19Feb2021/FullRun2_PerMassPoint_WithoutMa/M55_Cat/'
+
+  ## all masses training without m_a variables
+  # weightFile = '/afs/cern.ch/work/t/twamorka/H4G_Analysis/BDTTraining/PerformTraining/AllMasses/AllMassPoints_WithoutMa'
+  # outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/19Feb2021/FullRun2_AllMasses_WithoutMa/'
+
+  ## all masses training with m_a variables
+  # weightFile = '/afs/cern.ch/work/t/twamorka/H4G_Analysis/BDTTraining/PerformTraining/AllMasses/AllMassPoints_WithMa'
+  # outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/19Feb2021/FullRun2_AllMasses_WithMa/'
+
+  ## parametrized training
+  #weightFile = '/eos/user/b/bmarzocc/H4G/dataset_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd_newSignalWeights_parametrized'
+  #outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/19Feb2021/FullRun2_Parametrized/M55_Cat/'
+  
+  ## modified parametrized training
+  #weightFile = '/eos/user/b/bmarzocc/H4G/dataset_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd_newSignalWeights_parametrized_v2'
+  #outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/19Feb2021/dataset_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd_newSignalWeights_parametrized_v2/'
+  #weightFile = '/eos/user/b/bmarzocc/H4G/H4G_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd_m'
+  #outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/19Feb2021/H4G_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd_noCorrel/'
+  #weightFile = '/eos/user/b/bmarzocc/H4G/dataset_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd_newSignalWeights_parametrized_v2_noCorrel/'
+  #outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/19Feb2021/dataset_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd_newSignalWeights_parametrized_v2_noCorrel/'
+  #weightFile = '/afs/cern.ch/work/t/twamorka/H4G_Analysis/BDTTraining/PerformTraining/Parametrized_v3/'
+  #outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/19Feb2021/Parametrized_v3/'
+  #weightFile = '/afs/cern.ch/work/t/twamorka/H4G_Analysis/BDTTraining/PerformTraining/Training_v2_DiffMassRange_60/'
+  #outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/19Feb2021/H4G_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd_noCorrel_DiffMassRange/'
+  #weightFile = '/eos/user/b/bmarzocc/H4G/H4G_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd_m'
+  #outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/19Feb2021/H4G_PhoMVA_manyKinVars_aMass_fullRun2_DataMix_HighStat_kinWeight_dataSBScaling_MGPodd_bkgOdd_noCorrel_v2/'
+  weightFile = '/afs/cern.ch/work/t/twamorka/H4G_Analysis/BDTTraining/PerformTraining/Parametrized_v2_noCorrel_fullMassPoint/'
+  outDir = '/eos/user/t/twamorka/h4g_fullRun2/TrainingApplied_22Jan2021/19Feb2021/Parametrized_NoCorrel_FullMassRange/'
+  inputfile_signal = '/eos/user/t/twamorka/H4G_Signal_Campaign_18Jan2021/'
   inputfile_data = '/eos/user/t/twamorka/h4g_fullRun2/withSystematics/'
   inputfile_bkg = '/eos/user/t/twamorka/h4g_fullRun2/withSystematics/DataMix_HighStat/hadd/'
-  files = ['even','odd']
+  # inputfile_bkg = '/eos/user/t/twamorka/H4G_BkgMC/'
+  files = ['even']
   for m in mass:
       for y in year:
           for f in files:
-              arguments.append("{} {} {} {} {} {}".format(inputfile_signal+str(y)+'/hadd/signal_m_'+str(m)+'_'+f+'.root',m,doSystematics,y,weightFile+str(m)+'/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/signal_m_'+str(m)+'_'+str(y)+'_'+f+'.root'))
-  for m in mass:
-      for y in year:
-          arguments.append("{} {} {} {} {} {}".format(inputfile_data+str(y)+'/hadd/data_'+str(y)+'.root',60,doSystematics,y,weightFile+str(m)+'/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/data_'+str(y)+'.root'))
-  for m in mass:
-      for y in year:
-          for f in files:
-              arguments.append("{} {} {} {} {} {}".format(inputfile_bkg+'data_mix_'+str(y)+'_kinWeight_'+f+'.root',60,doSystematics,y,weightFile+str(m)+'/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/data_mix_'+str(y)+'_'+f+'.root'))
+              arguments.append("{} {} {} {} {} {} {} {}".format(inputDir, inputfile_signal+str(y)+'/hadd/signal_m_'+str(m)+'_'+f+'.root',m,m,doSystematics,y,weightFile+'/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/signal_m_'+str(m)+'_'+str(y)+'_'+f+'.root'))
+              arguments.append("{} {} {} {} {} {} {} {}".format(inputDir, inputfile_data+str(y)+'/hadd/data_'+str(y)+'.root',m,m,doSystematics,y,weightFile+'/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/data_'+str(y)+'.root'))
+              arguments.append("{} {} {} {} {} {} {} {}".format(inputDir, inputfile_bkg+'data_mix_'+str(y)+'_kinWeight_even.root',m,m,doSystematics,y,weightFile+'/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/data_mix_'+str(y)+'_even.root'))
+
+              # arguments.append("{} {} {} {} {} {} {}".format(inputDir, inputfile_signal+str(y)+'/hadd/signal_m_'+str(m)+'_'+f+'.root',m,doSystematics,y,weightFile+str(m)+'/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/signal_m_'+str(m)+'_'+str(y)+'_'+f+'.root'))
+  #for m in mass:
+      #for y in year:
+          #arguments.append("{} {} {} {} {} {} {} {}".format(inputDir, inputfile_data+str(y)+'/hadd/data_'+str(y)+'.root',60,m,doSystematics,y,weightFile+str(m)+'/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/data_'+str(y)+'.root'))
+  #for m in mass:
+      #or y in year:
+             #arguments.append("{} {} {} {} {} {} {} {}".format(inputDir, inputfile_data+str(y)+'/hadd/data_'+str(y)+'.root',m,m,doSystematics,y,weightFile+'/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/data_'+str(y)+'.root'))
+             #arguments.append("{} {} {} {} {} {} {} {}".format(inputDir, inputfile_bkg+'data_mix_'+str(y)+'_kinWeight_even.root',m,m,doSystematics,y,weightFile+'/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/data_mix_'+str(y)+'_even.root'))
+
+      # for y in year:
+  #         #arguments.append("{} {} {} {} {} {} {}".format(inputDir, inputfile_bkg+'DiPho_40to80_'+str(y)+'.root',60,doSystematics,y,weightFile+str(m)+'_newSignalWeights/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/DiPho_40to80_'+str(y)+'_skim_CR_D.root'))
+  #         #arguments.append("{} {} {} {} {} {} {}".format(inputDir, inputfile_bkg+'DiPho_80toInf_'+str(y)+'.root',60,doSystematics,y,weightFile+str(m)+'_newSignalWeights/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/DiPho_80toInf_'+str(y)+'_skim_CR_D.root'))
+  #         #arguments.append("{} {} {} {} {} {} {}".format(inputDir, inputfile_bkg+'DataDriven_'+str(y)+'_CR_A.root',60,doSystematics,y,weightFile+str(m)+'_newSignalWeights/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/DataDriven_'+str(y)+'_CR_A.root'))
+  #
+          # for f in files:
+          #     arguments.append("{} {} {} {} {} {} {}".format(inputDir, inputfile_bkg+'data_mix_'+str(y)+'_kinWeight_'+f+'.root',60,doSystematics,y,weightFile+str(m)+'/weights/TMVAClassification_BDTG.weights.xml',outDir+str(m)+'/data_mix_'+str(y)+'_'+f+'.root'))
+  # for y in year:
+  #        arguments.append("{} {} {} {} {} {} {}".format(inputDir, inputfile_data+str(y)+'/hadd/data_'+str(y)+'.root',60,doSystematics,y,weightFile+'/weights/TMVAClassification_BDTG.weights.xml',outDir+'data_'+str(y)+'.root'))
+  #        arguments.append("{} {} {} {} {} {} {}".format(inputDir, inputfile_bkg+'data_mix_'+str(y)+'_kinWeight_even.root',60,doSystematics,y,weightFile+'/weights/TMVAClassification_BDTG.weights.xml',outDir+'data_mix_'+str(y)+'_even.root'))
+
+  # for y in year:
+  #     for f in files:
+  #         arguments.append("{} {} {} {} {} {} {}".format(inputDir, inputfile_bkg+'data_mix_'+str(y)+'_kinWeight_'+f+'.root',60,doSystematics,y,weightFile+'/weights/TMVAClassification_BDTG.weights.xml',outDir+'data_mix_'+str(y)+'_'+f+'.root'))
 
   with open("arguments.txt", "w") as args:
      args.write("\n".join(arguments))
