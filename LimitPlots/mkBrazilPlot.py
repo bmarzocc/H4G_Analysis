df9b170e8783089cac65c363bca6409350609a2b
wamorkart <wamorkar.t@husky.neu.edu>
2021-05-09 19:03:44 +0200

    update brazil plot style and readme

--- LimitPlots/mkBrazilPlot.py
+++ LimitPlots/mkBrazilPlot.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams.update({'figure.max_open_warning': 0})
 
+plt.style.use('physics.mplstyle')
+
 import argparse
 parser =  argparse.ArgumentParser(description='H4G Brazil Plot Maker')
 parser.add_argument('-s','--style',dest='style',required=True,type=str)
@@ -44,6 +46,7 @@ obs = []
 ## Obtain values of expected and observed limits for each mass point
 for m in range (15,61): ## m(a) ranges from 15 to 60 GeV
     mass.append(m)
+    # print ("mass: ", m)
     if (style=="Asymptotic"):
         file_in = ROOT.TFile(inDir+"/higgsCombine_M"+str(m)+"_Asymptotic_FreezeMH.AsymptoticLimits.mH125.root","read")
         tree_in = file_in.Get("limit")
@@ -80,14 +83,16 @@ for m in range (15,61): ## m(a) ranges from 15 to 60 GeV
 
 
 ## Make Brazil Plot
+plt.figure()
 plt.ylim(ymin,ymax)
 if (norm==1):
     plt.ticklabel_format(axis='y',style='sci',scilimits=(0,4))
     plt.title(r'$\bf{CMS}$'+' $\it{'+str(label_left)+'}$',position=(0.15, 0.93)) ## Put CMS Preliminary inside the canvas; position=(0,0) is the bottom left and position=(1,1) is the top right
-    plt.ylabel(r'$\dfrac {\sigma (pp\rightarrow h)} {\sigma (SM)}  \quad X \quad  BR (h \rightarrow aa \rightarrow \gamma\gamma\gamma\gamma) $',labelpad=2) ## labelpad determines the distance between label axis and label values; a higher value will increase the distance
+    # plt.ylabel(r'$\dfrac {\sigma (pp\rightarrow h)} {\sigma (SM)}\times BR (h \rightarrow aa \rightarrow \gamma\gamma\gamma\gamma) $',labelpad=0.1) ## labelpad determines the distance between label axis and label values; a higher value will increase the distance
+    plt.ylabel(r'$\sigma (pp\rightarrow h)\times BR (h \rightarrow aa \rightarrow \gamma\gamma\gamma\gamma) / \sigma_{SM}$',labelpad=0.1) ## labelpad determines the distance between label axis and label values; a higher value will increase the distance
 else:
     plt.title(r'$\bf{CMS}$'+' $\it{'+str(label_left)+'}$',loc='left')    ## Put CMS Preliminary outside the canvas
-    plt.ylabel(r'$\sigma (pp\rightarrow h)  \quad X \quad  BR (h \rightarrow aa \rightarrow \gamma\gamma\gamma\gamma) \quad [fb] $',labelpad=2)
+    plt.ylabel(r'$\sigma (pp\rightarrow h)\times BR (h \rightarrow aa \rightarrow \gamma\gamma\gamma\gamma) \quad[fb] $',labelpad=2)
 
 # plt.ylabel(labelpad=10)
 # plt.ticklabel_format(axis='y',style='sci',scilimits=(0,4))
@@ -98,20 +103,15 @@ plt.xticks([15,20,25,30,35,40,45,50,55,60])
 # plt.grid(True)
 
 plt.plot(mass, exp_median,lineStyle='--', marker='',color='black',label='Median expected')
-plt.plot(mass, exp_down01sigma,lineStyle='-', marker='',color='yellowgreen',label=r'$ \pm 1\sigma$ expected')
-plt.plot(mass, exp_up01sigma,lineStyle='-', marker='',color='yellowgreen')
-plt.plot(mass, exp_down02sigma,lineStyle='-', marker='',color='yellow',label=r'$ \pm 2\sigma$ expected')
-plt.plot(mass, exp_up02sigma,lineStyle='-', marker='',color='yellow')
-
 if (blind==0): plt.plot(mass, obs,lineStyle='-', marker='',color='black',label='Observed')
 
-plt.fill_between(mass,exp_down02sigma , exp_up02sigma,facecolor='yellow')
-plt.fill_between(mass,exp_down01sigma , exp_up01sigma,facecolor='yellowgreen')
+plt.fill_between(mass,exp_down01sigma , exp_median,facecolor='#00CC00',lw=0,label="68% expected")
+plt.fill_between(mass,exp_down02sigma , exp_down01sigma,facecolor='#FFCC01',lw=0, label="95% expected")
 
-# if (norm==0):
-# else:
+plt.fill_between(mass,exp_median , exp_up01sigma,facecolor='#00CC00',lw=0)
+plt.fill_between(mass,exp_up01sigma , exp_up02sigma,facecolor='#FFCC01',lw=0)
 
-    # plt.yscale("logit")
+
 plt.xlabel('m(a) [GeV]')
 plt.legend(loc='upper right',framealpha=0)
 # plt.tight_layout()
@@ -124,8 +124,10 @@ plt.savefig(oDir+name+'.pdf')
 plt.savefig(oDir+name+'.png')
 
 plt.yscale("log")
+
 if (norm==0): plt.ylim(0,10)
-else: plt.ylim(1e-6,1e-3)
+else:
+    plt.ylim(1e-6,1e-3)
 
 plt.savefig(oDir+name+'_log.pdf')
 plt.savefig(oDir+name+'_log.png')
